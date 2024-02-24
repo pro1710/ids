d3cfa6d39fc76867209042690d24a63f305cb6d6
Roman Pykhnivskyi <pro1710@gmail.com>
2024-02-24 11:59:27 +0200

    common

--- ids_common.py
+++ ids_common.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+def drop_na_dups(df, verbose=False):
+    if verbose:
+        print(f'Before: dropna: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
+
+    df.dropna(axis=0, how='any', inplace=True)
+    df.drop_duplicates(subset=None, keep='first', inplace=True)
+
+    if verbose:
+        print(f'After: dropna: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
 
 def encode_text_dummy(df, name):
     dummies = pd.get_dummies(df[name])
@@ -10,63 +19,67 @@ def encode_text_dummy(df, name):
     df.drop(name, axis=1, inplace=True)
 
 
-def prepare_dataset(path_to_dataset, verbose=False):
-    df  = pd.read_csv(path_to_dataset, low_memory=False)
+dropped_by_ferrag = ['frame.time', 
+                    'ip.src_host', 
+                    'ip.dst_host', 
+                    'arp.src.proto_ipv4',
+                    'arp.dst.proto_ipv4', 
+                    'http.file_data',
+                    'http.request.full_uri',
+                    'icmp.transmit_timestamp',
+                    'http.request.uri.query', 
+                    'tcp.options',
+                    'tcp.payload',
+                    'tcp.srcport',
+                    'tcp.dstport', 
+                    'udp.port', 
+                    'mqtt.msg']
 
-    to_drop_columns = ['frame.time', 
-                'ip.src_host', 
-                'ip.dst_host', 
-                'arp.src.proto_ipv4',
-                'arp.dst.proto_ipv4', 
-                'http.file_data',
-                'http.request.full_uri',
-                'icmp.transmit_timestamp',
-                'http.request.uri.query', 
-                'tcp.options',
-                'tcp.payload',
-                'tcp.srcport',
-                'tcp.dstport', 
-                'udp.port', 
-                'mqtt.msg']
+def cleanup_dataset_ferrag(df):
+    df.drop(dropped_by_ferrag, axis=1, inplace=True)
 
-    df.drop(to_drop_columns, axis=1, inplace=True)
+def ferrag_preparation(df, verbose=False):
     if verbose:
-        print(f'drop: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
+        print(f'Before: shape={df.shape}')
+    cleanup_dataset_ferrag(df)
+    drop_na_dups(df, verbose)
+    if verbose:
+        print(f'After: shape={df.shape}')
 
-    to_drop_1_unique = ['icmp.unused',
+to_drop_1_unique = ['icmp.unused',
                     'http.tls_port',
                     'dns.qry.type',
                     'dns.retransmit_request_in',
+                    'mqtt.conack.flags',
                     'mqtt.msg_decoded_as',
                     'mbtcp.len',
                     'mbtcp.trans_id',
-                    'mbtcp.unit_id',
-                    'mqtt.conack.flags']
+                    'mbtcp.unit_id']
+
+mixed_zeros = ['mqtt.topic', 
+            #   'mqtt.conack.flags', 
+                'mqtt.protoname',
+                'dns.qry.name.len',
+                'http.request.method',
+                'http.referer', 
+                'http.request.version']
+
+
+
+
+
+def prepare_dataset(path_to_dataset, verbose=False):
+    df  = pd.read_csv(path_to_dataset, low_memory=False)
+
+    cleanup_dataset_ferrag(df)
 
     df.drop(to_drop_1_unique, axis=1, inplace=True)
-    if verbose:
-        print(f'drop: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
-
-    df.dropna(axis=0, how='any', inplace=True)
-    if verbose:
-        print(f'dropna: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
-
-    to_fix_columns = ['mqtt.topic', 
-                    #   'mqtt.conack.flags', 
-                      'mqtt.protoname',
-                      'dns.qry.name.len',
-                      'http.request.method',
-                      'http.referer', 
-                      'http.request.version']
     
-    for col in to_fix_columns:
-        df.loc[(df[col] == '0.0') | (df[col] == '0x00000000'), col] = '0'
+    drop_na_dups(df, verbose)
+
+    for col in mixed_zeros:
+        df.loc[(df[col] == '0.0') | (df[col] == '0x00000000'), col] = '0.0'
         encode_text_dummy(df, col)
-
-
-    df.drop_duplicates(subset=None, keep='first', inplace=True)
-    if verbose:
-        print(f'drop_duplicates: NA: {df.isnull().sum().sum()}, DUPS: {df.duplicated().sum()}', )
 
     df['Attack_type'] = df['Attack_type'].astype('category')
 
